dd6b9d3a4dc09d127f58ea22f4c06b6f8596eea3
xOVHx <74750962+xOVHx@users.noreply.github.com>
2024-06-25 06:13:19 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/_app.tsx
@@ -0,0 +1,147 @@
+import { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
+import Head from 'next/head';
+
+import NProgress from 'components/nprogress';
+
+import fetcher from 'lib/fetcher';
+
+import 'fonts.css';
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <SWRConfig value={{ fetcher }}>
+      <Head>
+        <title>Crowd Signal Hack</title>
+        <meta name='description' content='Easily rack up votes for any Crowd Signal (previously Poll Daddy) public survey.' />
+      </Head>
+      <NProgress />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        ::selection {
+          background-color: var(--selection);
+        }
+
+        *,
+        *:before,
+        *:after {
+          box-sizing: inherit;
+        }
+
+        html {
+          height: 100%;
+          box-sizing: border-box;
+          touch-action: manipulation;
+          font-feature-settings: 'kern';
+        }
+
+        body {
+          position: relative;
+          min-height: 100%;
+          margin: 0;
+        }
+
+        html,
+        body {
+          font-size: 16px;
+          line-height: 1.45;
+          font-family: var(--font-sans);
+          text-rendering: optimizeLegibility;
+          -webkit-font-smoothing: subpixel-antialiased;
+          -moz-osx-font-smoothing: grayscale;
+          background-color: var(--background);
+          color: var(--on-background);
+        }
+
+        h2 {
+          font-size: 1.2rem;
+          margin: 1rem 0;
+        }
+
+        .wrapper {
+          max-width: calc(var(--page-width) + 2 * 1rem);
+          padding: 0 1rem;
+          margin: auto;
+        }
+
+        .wrapper > h2:first-child {
+          margin-top: 0;
+        }
+
+        p {
+          margin: 1rem 0;
+        }
+
+        a {
+          cursor: pointer;
+          text-decoration: none;
+          color: var(--accents-5);
+          transition: color 0.2s ease 0s;
+        }
+
+        a:hover,
+        a.active {
+          color: var(--on-background);
+        }
+
+        a.active {
+          cursor: not-allowed;
+        }
+          
+        .loading {
+          background-image: linear-gradient(
+            270deg,
+            var(--accents-1),
+            var(--accents-2),
+            var(--accents-2),
+            var(--accents-1)
+          );
+          background-size: 400% 100%;
+          -webkit-animation: loadingAnimation 8s ease-in-out infinite;
+          animation: loadingAnimation 8s ease-in-out infinite;
+          cursor: wait;
+        }
+
+        @keyframes loadingAnimation {
+          0% {
+            background-position: 200% 0;
+          }
+          to {
+            background-position: -200% 0;
+          }
+        }
+      `}</style>
+      <style jsx global>{`
+        :root {
+          --font-sans: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+            'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
+            'Droid Sans', 'Helvetica Neue', sans-serif;
+          --font-mono: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+
+          --page-width: 800px;
+          
+          --primary: #c84702;
+          --on-primary: #fff;
+          --background: #000;
+          --on-background: #fff;
+          --error: #cf6679;
+          --on-error: #000;
+
+          --accents-1: #111;
+          --accents-2: #333;
+          --accents-3: #444;
+          --accents-4: #666;
+          --accents-5: #888;
+          --accents-6: #999;
+
+          --shadow-small: 0 0 0 1px var(--accents-2);
+          --shadow-medium: 0 0 0 1px var(--accents-2);
+          --shadow-large: 0 0 0 1px var(--accents-2);
+
+          --selection: #c84702;
+        }
+      `}</style>
+    </SWRConfig>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,311 @@
+import { FormEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import Bottleneck from 'bottleneck';
+import NProgress from 'nprogress';
+import { to } from 'await-to-js';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+
+import { Option } from 'pages/api/options';
+
+import Empty from 'components/empty';
+
+const BOTTLENECK = { maxConcurrent: 100 };
+
+function num(n: unknown, d: number): number {
+  return Number.isNaN(Number(n)) ? d : Number(n);
+}
+
+export default function IndexPage(): JSX.Element {
+  const { push, query: { p, o, v } } = useRouter();
+  const poll = useMemo(() => num(p, 11033591), [p]);
+  const option = useMemo(() => num(o, 50654329), [o]);
+  const votes = useMemo(() => num(v, 1000), [v]);
+
+  const [count, setCount] = useState(0);
+  const [going, setGoing] = useState(false);
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    if (going) {
+      NProgress.set(count / votes);
+    } else {
+      NProgress.done();
+    }
+  }, [going, count, votes]);
+  const limiter = useRef(new Bottleneck(BOTTLENECK));
+  useEffect(() => {
+    limiter.current.on('done', () => setCount((prev) => prev + 1));
+    limiter.current.on('failed', async (error, jobInfo) => {
+      setMessage(`Vote (${jobInfo.options.id}) failed: ${error}`);
+      if (jobInfo.retryCount < 10) {
+        setMessage(`Retrying vote (${jobInfo.options.id}) in 100ms...`);
+        return 100;
+      }
+      return;
+    });
+    limiter.current.on('retry', (_, jobInfo) => {
+      setMessage(`Now retrying vote (${jobInfo.options.id})...`);
+    });
+  }, [going]);
+  const start = useCallback(async (e: FormEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setGoing(true);
+    setCount(0);
+    const url = `/api/poll?poll=${poll}&option=${option}`;
+    setMessage(`Forging ${votes} votes...`);
+    await to(Promise.all(Array(votes).fill(null).map((_, idx) => limiter.current.schedule({ id: idx.toString() }, () => fetch(url)))));
+    setMessage(`Forged ${votes} votes.`);
+    setGoing(false);
+  }, [poll, option, votes]);
+  const stop = useCallback(async (e: FormEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setMessage(`Canceling ${votes - count} votes...`);
+    await to(limiter.current.stop());
+    limiter.current = new Bottleneck(BOTTLENECK);
+    setMessage(`Canceled ${votes - count} votes.`);
+  }, [votes, count]);
+
+  const { data } = useSWR<Option[]>(`/api/options?poll=${poll}`);
+
+  return (
+    <main className='wrapper'>
+      <header>
+        <h1>Crowd Signal Hack</h1>
+        <p>Easily rack up votes for any Crowd Signal (previously Poll Daddy) public survey.</p>
+        <p>Simply click the “Start” button, leave this tab open, and watch the votes!</p>
+        <p>Created and <a href='https://github.com/nicholaschiang/poll' target='_blank' rel='noopener noreferrer'>open sourced</a> by <a href='https://nicholaschiang.com' target='_blank' rel='noopener noreferrer'>Nicholas Chiang</a>; it was just too easy.</p>
+      </header>
+      <div className='field'>
+        <label htmlFor='poll'>Poll URL</label>
+        <input 
+          id='poll'
+          type='url' 
+          className='textfield'
+          placeholder='Ex: https://poll.fm/10924113' 
+          value={`https://poll.fm/${poll}`} 
+          disabled={going}
+          onChange={(e) => push(`/?p=${e.currentTarget.value.split('https://poll.fm/').pop()}&v=${votes}&o=${option}`)} 
+        />
+      </div>
+      <ul className='field options'>
+        <label>Option</label>
+        {data && data.map(({ id, label }) => (
+          <li key={id}>
+            <input
+              id={id}
+              type='radio' 
+              value={id} 
+              disabled={going}
+              className='radio'
+              checked={option === Number(id)}
+              onChange={() => push(`/?p=${poll}&v=${votes}&o=${id}`)} 
+            />
+            <label htmlFor={id}>{label}</label>
+          </li>
+        ))}
+        {!data && Array(5).fill(null).map((_, idx) => <li key={idx} className='loading' />)}
+        {data && !data.length && <Empty>Enter a valid poll URL above to get started.</Empty>}
+      </ul>
+      <div className='field'>
+        <label htmlFor='votes'>Number of votes</label>
+        <input
+          id='votes'
+          type='number'
+          className='textfield'
+          placeholder='Ex: 1000'
+          value={votes}
+          disabled={going}
+          onChange={(e) => push(`/?p=${poll}&v=${e.currentTarget.value}&o=${option}`)}
+        />
+      </div>
+      <div className='buttons'>
+        <button 
+          className='reset' 
+          type='button' 
+          disabled={going || !data?.some((d) => Number(d.id) === option)} 
+          onClick={start}
+        >
+          Start
+        </button>
+        <button 
+          className='reset' 
+          type='button' 
+          disabled={!going} 
+          onClick={stop}
+        >
+          Stop
+        </button>
+      </div>
+      <p>{(count / votes * 100).toFixed(2)}% progress; {count}/{votes} votes forged. <span className='message'>{message}</span></p>
+      <style jsx>{`
+        main {
+          margin: 2rem auto;
+        }
+
+        header {
+          text-align: center;
+          margin: 2rem 0;
+        }
+
+        .message {
+          color: var(--accents-5);
+        }
+
+        .field {
+          border: 1px solid var(--accents-2);
+          background: var(--accents-1);
+          border-radius: 4px;
+          padding: 1rem;
+          margin: 1rem 0;
+        }
+
+        ul {
+          list-style: none;
+        }
+
+        ul :global(.empty) {
+          margin-top: 1rem;
+          height: ${1.25 * 5 + 1 * 4}rem;
+        }
+
+        li {
+          display: flex;
+          align-items: center;
+          margin: 1rem 0;
+          height: 1.25rem;
+        }
+
+        li.loading {
+          width: 100%;
+          border-radius: 4px;
+        }
+
+        li:last-child {
+          margin-bottom: 0;
+        }
+
+        li label,
+        li .radio {
+          margin: 0;
+          text-transform: unset;
+        }
+
+        .radio {
+          position: absolute;
+          opacity: 0;
+        }
+        .radio + label:before {
+          content: '';
+          background: var(--accents-1);
+          border-radius: 100%;
+          border: 1px solid var(--accents-2);
+          display: inline-block;
+          width: 1rem;
+          height: 1rem;
+          position: relative;
+          top: 0.25rem;
+          margin-right: 0.75rem; 
+          vertical-align: top;
+          cursor: pointer;
+          text-align: center;
+          transition: all 0.2s ease 0s;
+        }
+        .radio:checked + label:before {
+          background-color: var(--primary);
+          box-shadow: inset 0 0 0 4px var(--accents-1);
+          border-color: var(--primary);
+          outline: none;
+        }
+        .radio:disabled + label:before {
+          border-color: var(--accents-2);
+          background: var(--accents-1);
+          filter: grayscale(1);
+          cursor: not-allowed;
+        }
+        .radio:checked:disabled + label:before {
+          background-color: var(--accents-2);
+        }
+        .radio + label:empty:before {
+          margin-right: 0;
+        }
+
+        .buttons {
+          display: flex;
+          margin: 0 0 2rem;
+        }
+
+        label {
+          font-size: 1rem;
+          font-weight: 600;
+          text-transform: uppercase;
+          margin-bottom: 0.5rem;
+          display: block;
+        }
+        
+        .textfield {
+          border-radius: 4px;
+          border: 1px solid var(--accents-2);
+          box-shadow: none;
+          box-sizing: border-box;
+          display: block;
+          padding: 1rem;
+          font-family: var(--font-sans);
+          font-size: 1rem;
+          outline: currentcolor none 0px;
+          width: 100%;
+          color: var(--on-background);
+          background-color: transparent;
+          caret-color: var(--on-background);
+          text-overflow: ellipsis;
+          appearance: none;
+          transition: border 0.2s ease 0s;
+        }
+
+        .textfield:focus,
+        .textfield:active {
+          border: 1px solid var(--primary);
+        }
+
+        button {
+          border: 1px solid var(--primary);
+          background: var(--primary);
+          border-radius: 4px;
+          color: var(--on-primary);
+          font-family: var(--font-sans);
+          font-weight: 700;
+          font-size: 1.25rem;
+          padding: 1rem 2rem;
+          margin: 0 0.5rem;
+          width: 100%;
+          cursor: pointer;
+          transition: transform 0.2s ease 0s;
+          text-transform: uppercase;
+        }
+
+        button:first-child {
+          margin-left: 0;
+        }
+
+        button:last-child {
+          margin-right: 0;
+        }
+
+        button:hover {
+          transform: scale(.99);
+        }
+
+        button:disabled,
+        input:disabled {
+          cursor: not-allowed;
+          border-color: var(--accents-2);
+          background: var(--accents-1);
+          color: var(--on-background);
+          filter: grayscale(1);
+          transform: translateZ(0px);
+          backface-visibility: hidden;
+        }
+      `}</style>
+    </main>
+  );
+}
